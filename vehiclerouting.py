c67b18b10c7dcb64c830d71ed842fcc36c293e77
Tomaso De Ponti <tomasodp@gmail.com>
2020-11-09 22:20:21 +0100

    November 9 tomaso evening push 1

--- vehiclerouting.py
+++ vehiclerouting.py
@@ -1,5 +1,6 @@
 
 # -*- coding: utf-8 -*-
+
 """
 @author: tdeponti
 """
@@ -11,7 +12,8 @@ import time
 from gurobipy import Model,GRB,LinExpr
 import pickle
 from copy import deepcopy
-from plotter import do_plot
+
+#from plotter import do_plot
 
 # Get path to current folder
 cwd = os.getcwd()
@@ -19,9 +21,9 @@ print(cwd)
 # Get all instances
 full_list = os.listdir(cwd)
 
-# instance name
-instance_name = '\pvr.xlsx'
-
+instance_name ='/pvrold.xlsx'
+#instance_name = '/database/pvr.xlsx'
+#instance_name = '/pvr.xlsx'
 # Load data for this instance
 edges= pd.read_excel(cwd + instance_name, sheet_name='data')
 print("edges", edges)
@@ -33,48 +35,50 @@ custumerdemand=1
 maxpayload=3
 costperkm=0.5
 maxdrones=10
-maxrange=15
+maxrange=200
+speed=150
+takeofftime=2
+landingtime=2
+unloadingtime=5
+
+
+
 
 startTimeSetUp = time.time()
 model = Model()
-
+maxnode = max(edges['From']+1)
 #################
 ### VARIABLES ###
 #################
+
 xk = {}
-for k in range(1,maxdrones+1):
-    xk[k] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY,name="xk[%s]" % (k))
 x = {}
 for k in range(1,maxdrones+1):
+    xk[k] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY,name="xk[%s]" % (k))
     for i in range(0, len(edges)):
         x[edges['From'][i], edges['To'][i],k] = model.addVar(lb=0, ub=1, vtype=GRB.BINARY,name="x[%s,%s,%s]" % (edges['From'][i], edges['To'][i],k))
-
 s = {}
 for i in range(1, max(edges['From']+1)):
     for k in range(1, maxdrones + 1):
-        s[i,k] = model.addVar(lb=0,ub=100,vtype=GRB.INTEGER, name="s[%s,%s]"%(i,k))
+        s[i,k] = model.addVar(lb=0,ub=K,vtype=GRB.INTEGER,name="s[%s,%s]"%(i,k))
 model.update()
 
 ###################
 ### CONSTRAINTS ###
 ###################
-print(len(edges) - 1)
-print(edges['From'][len(edges) - 1])
-print(range(1, edges['From'][len(edges) - 1]))
-#for i in range(1, edges['From'][len(edges) - 1]):
-
-for i in range(1, max(edges['From']+1)):
-    print("Node",i)
+#print(len(edges) - 1)
+#print(edges['From'][len(edges) - 1])
+#print(range(1, edges['From'][len(edges) - 1]))
+for i in range(1, maxnode):
+    #print("Node",i)
     idx_this_node_out = np.where(edges['From'] == i)[0]
     idx_this_node_in = np.where(edges['To'] == i)[0]
-
     thisLHS = LinExpr()
-    if len(idx_this_node_out) > 0:
-        for k in range(1, maxdrones + 1):
-            for j in range(0, len(idx_this_node_out)):
-                thisLHS += x[i, edges['To'][idx_this_node_out[j]],k]
-                thisLHS -= x[edges['From'][idx_this_node_in[j]], i, k]
-            model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=0,name='node_flow_' + str(i)+str(k))
+    for k in range(1, maxdrones + 1):
+        for j in range(0, len(idx_this_node_out)):
+            thisLHS += x[i, edges['To'][idx_this_node_out[j]],k]
+            thisLHS -= x[edges['From'][idx_this_node_in[j]], i, k]
+        model.addConstr(lhs=thisLHS, sense=GRB.EQUAL, rhs=0,name='node_flow_' + str(i)+"i"+str(k)+"k")
     # if len(idx_this_node_in) > 0:
     #     for j in range(0, len(idx_this_node_in)):
     #         for k in range(1, maxdrones + 1):
@@ -109,7 +113,7 @@ for k in range(1, maxdrones + 1):
     thisLHS2 -= xk[k]
     model.addConstr(lhs=thisLHS1, sense=GRB.EQUAL, rhs=0, name='drone_out_' + str(k))
     model.addConstr(lhs=thisLHS2, sense=GRB.EQUAL, rhs=0, name='drone_in_' + str(k))
-    model.addConstr(lhs=thisLHS, sense=GRB.LESS_EQUAL, rhs=maxpayload, name='drone_payload_' + str(k))
+    model.addConstr(lhs=thisLHS, sense=GRB.LESS_EQUAL, rhs=maxpayload+1, name='drone_payload_' + str(k))
     model.addConstr(lhs=thisrangeLHS, sense=GRB.LESS_EQUAL, rhs=maxrange, name='drone_range_' + str(k))
 
 
@@ -117,23 +121,26 @@ thisLHS = LinExpr()
 count = 0
 for i in range(1, max(edges['From']+1)):
     for j in range (1, max(edges['From']+1)):
-        print(count)
+        #print(count)
         if i != j & j!=1:
             for k in range(1, maxdrones + 1):
                 #can add here an if for impossible edges
                 thisLHS= LinExpr()
-                thisLHS= s[i,k]-s[j,k]+edges['Distance'][count]-K*(1-x[i,j,k])#[i+j-1]#+K*(1-x[i,j])#+edges['Distance'][1]
+                thisLHS= s[i,k]-s[j,k]+((edges['Distance'][count])*speed/60+takeofftime+landingtime+unloadingtime)-K*(1-x[i,j,k])#[i+j-1]#+K*(1-x[i,j])#+edges['Distance'][1]
 
-                model.addConstr(lhs=thisLHS, sense = GRB.LESS_EQUAL, rhs=0, name='time_' + str(i)+str(j)+str(k))
+                model.addConstr(lhs=thisLHS, sense = GRB.LESS_EQUAL, rhs=0, name='time_' + str(i)+"i"+str(j)+"j"+str(k)+"k")
             count = count + 1
 
 
 model.update()
 obj = LinExpr()
-
-for i in range(0, len(edges)):
-    for k in range(1, maxdrones + 1):
-        obj += edges['Distance'][i] * x[edges['From'][i], edges['To'][i],k]*costperkm
+count=0
+for i in range(1, maxnode):
+    for j in range(1,maxnode):
+        if i != j:
+            for k in range(1, maxdrones + 1):
+                obj += edges['Distance'][count] * x[i,j,k]*costperkm #+ s[i,k]
+            count += 1
 for k in range(1,maxdrones+1):
     obj += xk[k]*droneFC
 
@@ -142,21 +149,20 @@ for k in range(1,maxdrones+1):
 model.setObjective(obj, GRB.MINIMIZE)
 model.update()
 model.write('model_formulation2.lp')
-
+print("------STARTING OPTIMIZATION--------")
 model.optimize()
 endTime = time.time()
 
+fullsolution = []
 solution = []
-activedrones =[]
-
 for v in model.getVars():
+    fullsolution.append([v.varName, v.x])
     if v.x==1:
         solution.append([v.varName])
     #if v.xk==1:
     #    activedrones.append([v.varName])
-print(activedrones)
 print(solution)
-
+print(fullsolution)
 
 def solution_to_excel(solution):
 
@@ -176,30 +182,11 @@ def solution_to_excel(solution):
     solution_df.to_excel(cwd + "\database\solution.xlsx", 'data')
 
 
-route_complete = False
+#Comment out because not working
 
-do_plot()
-solution_to_excel(solution)
+#do_plot()
+#solution_to_excel(solution)
 
-
-# while route_complete is False:
-#     # Connections from current node
-#     idx_this_node_out = np.where(edges['From'] == current_node)[0]
-#     # print(idx_this_node_out)
-#     for i in range(0, len(idx_this_node_out)):
-#         if x[current_node, edges['To'][idx_this_node_out[i]]].x >= 0.99:
-#             path.append(edges['To'][idx_this_node_out[i]])
-#             current_node = edges['To'][idx_this_node_out[i]]
-#
-#             if current_node == sinko:
-#             #if current_node == 1:
-#                 route_complete = True
-#                 break
-#             else:
-#                 break
-#     print("running...")
-#
-# print(path)
 
 
 
